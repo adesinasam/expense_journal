b5422ea4a661d593aa670191bd88839e437b7c25
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-06-07 15:15:46 +0100

    Update Role

--- expense_journal/hooks.py
+++ expense_journal/hooks.py
@@ -127,7 +127,7 @@ doc_events = {
 # }
 
 
-fixtures = ["Workflow", "Workflow State", "Workflow Action Master",
+fixtures = ["Workflow", "Workflow State", "Workflow Action Master", "Role",
 	{
 		"dt": "Print Format",
 		"filters": [
