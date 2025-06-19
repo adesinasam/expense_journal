87fb7ceea4c469f0e1c6febec57cf90154662e43
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-06-19 14:37:30 +0100

    Update expense_entry.py

--- expense_journal/expense_journal/doctype/expense_entry/expense_entry.py
+++ expense_journal/expense_journal/doctype/expense_entry/expense_entry.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe import _, throw  # Add this import
 
 import erpnext
 from erpnext.accounts.utils import (
